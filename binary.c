8d3e400ec19e5c7b40dcc57da34ac16c491ab9f1
negia7265 <96509253+negia7265@users.noreply.github.com>
2022-02-07 18:56:39 +0530

    Add files via upload

--- binary.c
+++ binary.c
@@ -1,14 +1,21 @@
 #include<stdio.h>
+void binary(int n)
+{
+    int i[50],sum=0,k=0;
+ while(n!=0)
+ {
+      sum=n%2;
+      n=n/2;
+      i[k]=sum;
+      k++;
+ }
+ for(int j=k-1;j>=0;j--)
+ printf("%d",i[j]);
+}
 void main()
 {
-    int n,rem,sum=0;
-    printf("enter number");
-    scanf("%d",&n);
-    for(int i=1;n>0;i=i*10)
-    {
-        rem=n%2;
-        n=n/2;
-        sum=sum+rem*i;
-    }
-    printf("%d",sum);
-}
+   int n;
+   printf("enter");
+   scanf("%d",&n);
+   binary(n);
+   }
